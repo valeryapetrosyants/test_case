224544f7ae16721055ec5c9322fb90bfd96dd826
Waleriya Kudreva <valerya.petrosyants@yandex.ru>
2022-09-15 14:30:24 +0300

    test1111

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -2,12 +2,12 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+@Deprecated import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
+        open("https://www.google.ru/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
